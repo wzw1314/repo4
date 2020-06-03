a3cf96c616d415b1af98de5d6d3467e0f64b394a
zhangsan <zhangsan@qq.com>
2020-06-03 17:51:44 +0800

    将git_Project1提交的git

---
+++ git_Project1/src/cn/itcast/Test.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/**
+ * @author 王竹威
+ * @date 2020/6/3 17:45
+ * @school nuaa
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
